cf12eca81aef0002fa89405a7a9ccd3d5bb5ec04
Edel O'Shea <b00139077@mytudublin.ie>
2020-10-24 06:59:08 -0400

    commit to github

---
+++ CrytoHack/General/XOR/XOR Properties/solution.py
@@ -0,0 +1,19 @@
+from pwn import xor
+
+# KEY1 = a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313
+# KEY2 ^ KEY1 = 37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e
+# KEY2 ^ KEY3 = c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1
+# FLAG ^ KEY1 ^ KEY3 ^ KEY2 = 04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf
+
+key1 = bytes.fromhex('a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313')
+
+key2 = xor(bytes.fromhex('37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e'), key1)
+
+key3 = xor(bytes.fromhex('c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1'), key2) 
+
+key1_XOR_key3_XOR_key2 = xor(key1, key2, key3)
+
+flag = xor(bytes.fromhex('04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf'), key1_XOR_key3_XOR_key2)
+
+print(flag)
+
