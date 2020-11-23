4584e58088e0f7f5954080b2e1d7e95eb8342c3a
Edel O'Shea <b00139077@mytudublin.ie>
2020-11-23 16:31:49 -0500

    23/11

--- CrytoHack/Mathmatics/Modular Math/Chinese Remainder Theorem/solution.py
+++ CrytoHack/Mathmatics/Modular Math/Chinese Remainder Theorem/solution.py
@@ -1,10 +1,31 @@
 #x ≡ 2 mod 5
 #x ≡ 3 mod 11
 #x ≡ 5 mod 17 
-
 #x ≡ a mod 935
 
-for x in range (0,10000000000): 
-        if pow(2,5) == x:
-            print (x)
+from functools import reduce
 
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a*b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+ 
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
+
+
+n = [5, 11, 17]
+a = [2, 3, 5]
+
+print(chinese_remainder(n, a))
---
+++ CrytoHack/Mathmatics/Probability/Jack's Birthday Hash/solution.py
@@ -0,0 +1,5 @@
+# JACK(secret) = 01011001101
+
+prob = (0.5**11) 
+
+print (prob)
